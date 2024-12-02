9fcebc9719b982d3324dda07adfe14aa9d423356
hazel <dev.hhyerim@gmail.com>
2024-12-03 00:09:09 +0900

    [feat] create random avatar component

--- app/(route)/page.tsx
+++ app/(route)/page.tsx
@@ -1,6 +1,14 @@
 import '@/app/_styles/test.scss';
+
+import RandomAvatar from '../_components/RandomAvatar';
+
 const Main = () => {
-  return <div className="main-page">Main page</div>;
+  return (
+    <div className="main-page">
+      Main page
+      <RandomAvatar />
+    </div>
+  );
 };
 
 export default Main;
